db5709c7eb460985b692672b1914ce7e34aa5563
Juha Lindstedt <juha@pakastin.fi>
2022-12-03 00:16:52 +0200

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
-const server = require('http').createServer();
+const { NODE_PORT } = process.env;
+
+const express = require('express');
+
+const app = express();
+const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
   cors: {
     origin: 'https://car.js.org',
-    methods: ['GET'],
     credentials: true
   }
 });
 
-server.listen(80);
+server.listen(NODE_PORT, (err) => {
+  if (err) {
+    throw new Error(err);
+  }
+  console.log(`Listening port ${NODE_PORT}`);
+});
 
 io.on('connection', (socket) => {
   const { id } = socket;
@@ -36,28 +45,37 @@ io.on('connection', (socket) => {
       points
     } = params;
 
+    const newParams = {
+      x,
+      y,
+      xVelocity,
+      yVelocity,
+      power,
+      reverse,
+      angle,
+      angularVelocity,
+      isTurningLeft,
+      isTurningRight,
+      isThrottling,
+      isReversing,
+      isHit,
+      isShot,
+      isShooting,
+      lastShootAt,
+      name,
+      points
+    };
+
+    if (isHit || isShot) {
+      newParams.x = Math.round(Math.random() * 1500);
+      newParams.y = Math.round(Math.random() * 1500);
+      newParams.isHit = false;
+      newParams.isShot = false;
+    }
+
     socket.broadcast.emit('params', {
       id,
-      params: {
-        x,
-        y,
-        xVelocity,
-        yVelocity,
-        power,
-        reverse,
-        angle,
-        angularVelocity,
-        isTurningLeft,
-        isTurningRight,
-        isThrottling,
-        isReversing,
-        isHit,
-        isShot,
-        isShooting,
-        lastShootAt,
-        name,
-        points
-      }
+      params: newParams
     });
   });
 
